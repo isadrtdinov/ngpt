c445f82d2a3dba612062df4cfedc676301f0f3cc
ilsa767h <ilsa767h@c1.capella.hpc.tu-dresden.de>
2025-09-09 16:21:33 +0200

    add entropy logger

--- entropy.py
+++ entropy.py
@@ -1,51 +1,77 @@
 import torch
-from spherical import to_spherical
+import numpy as np
+from collections import deque
+from scipy.special import loggamma
 from sklearn.neighbors import NearestNeighbors
 
 
-def expected_log_jacobian(angles):
-    assert angles.shape[-1] >= 3
-    log_sines = angles[..., 1:-1].sin().log().mean(dim=-2)
-    shape = (1, ) * (len(log_sines.shape) - 1) + (-1, )
-    mults = torch.arange(angles.shape[-1] - 2, 0, -1).reshape(shape)
-    log_jacobian = (log_sines * mults).sum(dim=-1)
-    return log_jacobian
+def to_spherical(x):
+    """
+    Convert cartesian coordinates to spherical
+    Input: x: torch.Tensor of size (..., D)
+    Output: radii: corresponding radii of size (...)
+            angles: corresponding angle vectors of size (..., D - 1)
+    """
+    denom = torch.flip(
+        torch.cumsum(
+            torch.flip(x.square(), dims=(-1, )), dim=-1
+        ), dims=(-1, )
+    )
+    angles = torch.atan2(denom[..., 1:].sqrt(), x[..., :-1])
+    angles[..., -1] = torch.atan2(x[..., -1], x[..., -2])
+    angles[..., -1] = torch.where(angles[..., -1] > 0, angles[..., -1], angles[..., -1])
+    radii = denom[..., 0]
+    return radii, angles
 
 
-def entropy_kozachenko_leonenko(weights, mode='scale', effective_dim=None):
-    assert mode in ['isotropic', 'scale', 'covariance']
-    N, D = weights.shape[1:]
-    dim = D if effective_dim is None else effective_dim
-    entropy = []
+class EntropyLogger(object):
+    def __init__(self, queue_size, allow_cdist=False, thr=1e-8):
+        self.queue_size = queue_size
+        self.angles = deque(maxlen=queue_size)
+        self.radii = deque(maxlen=queue_size)
+        self.allow_cdist = allow_cdist
+        self.thr = thr
 
-    for weight in weights:
-        normed_weight = weight - weight.mean(dim=0, keepdim=True)
-        if mode == 'scale':
-            std = normed_weight.std(dim=0, keepdim=True)
-            normed_weight = normed_weight / std
-        elif mode == 'covariance':
-            sigma = normed_weight.T @ normed_weight / (N - 1)
-            U, S, V = torch.svd(sigma)
-            A = U * (1 / S.sqrt()) @ U.T
-            normed_weight = weight @ A
+    def add_weights(self, weights):
+        radius, angle = to_spherical(weights)
+        self.angles.append(angle)
+        self.radii.append(radius.item())
 
-        nb = NearestNeighbors(n_neighbors=2).fit(normed_weight)
-        dists, _ = nb.kneighbors(normed_weight)
-        value = dim * np.log(dists[..., 1]).mean() + dim / 2 * np.log(np.pi) - loggamma(dim / 2 + 1) + \
-                np.log(N - 1) + np.euler_gamma
+    def get_radius(self):
+        return torch.tensor(list(self.radii)).mean().item()
 
-        if mode == 'scale':
-            value += std.log().sum()
-        elif mode == 'covariance':
-            value += 0.5 * S.log().sum()
-        entropy.append(value)
+    def get_entropy(self):
+        if len(self.angles) < self.queue_size:
+            spherical_entropy, log_radius = np.nan, np.nan
+        else:
+            dim = self.angles[0].shape[0]
+            N = self.queue_size
 
-    return torch.tensor(entropy)
+            if self.allow_cdist:
+                angles = torch.stack(list(self.angles), dim=0)
+                distances = torch.cdist(angles, angles).numpy()
+            else:
+                distances = np.zeros((N, N))
+                for i in range(N):
+                    for j in range(i):
+                        distances[i, j] = torch.norm(self.angles[i] - self.angles[j]).item()
+                distances = distances + distances.T
 
+            knn_distances = np.sort(distances, axis=0)[1]
+            knn_distances = np.clip(knn_distances, a_min=self.thr, a_max=None)
+            angles_entropy = dim * np.log(knn_distances).mean() + dim / 2 * np.log(np.pi) - loggamma(dim / 2 + 1) + \
+                         np.log(N - 1) + np.euler_gamma
 
-def spherical_entropy(weights):
-    normalized_weights = weights / weights.norm(dim=-1, keepdim=True)
-    angles = sp.to_spherical(normalized_weights)
-    angles_ent = entropy_kozachenko_leonenko(angles[..., 1:])
-    log_jac = expected_log_jacobian(angles)
-    return angles_ent + log_jac
+            log_jacobian = 0.0
+            mults = torch.arange(dim - 1, 0, -1)
+            for i in range(N):
+                log_jacobian += (
+                    mults *
+                    self.angles[i][:-1].sin().clip(min=self.thr).log()
+                ).sum().item() / N
+
+            spherical_entropy = angles_entropy + log_jacobian
+            radius = self.get_radius()
+            log_radius = dim * np.log(radius)
+
+        return spherical_entropy, log_radius
